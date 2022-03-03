a5237defd5a2ff7592fbafe0effe47b3af6c2175
wood <2191964232@qq.com>
2022-03-03 08:04:07 +0800

    存在重复元素题目

---
+++ src/初级算法/数组/四_存在重复元素/Solution.java
@@ -0,0 +1,13 @@
+package 初级算法.数组.四_存在重复元素;
+
+public class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
